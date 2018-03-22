862fd00fa53842ab62c180b5692cfde72ddc8791
Parker Smith <parker@parkersmith.us>
2018-03-21 20:45:25 -0600

    First run implementation

--- src/Converter.java
+++ src/Converter.java
@@ -1,6 +1,24 @@
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+
 /**
  * Created by parker on 3/21/18.
  */
 public class Converter {
+    public static String hex_to_base64(String input)
+    {
+        byte[] output;
 
+        try
+        {
+            output = Base64.encodeBase64(Hex.decodeHex(input));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong decoding hex");
+            return null;
+        }
+
+        return output.toString();
+    }
 }
--- test/ConverterTest.java
+++ test/ConverterTest.java
@@ -11,8 +11,8 @@ class ConverterTest {
     void test_hex_to_base64_works()
     {
         String input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        byte[] output = Converter.hex_to_base64(input.getBytes());
+        String output = Converter.hex_to_base64(input);
 
-        Assertions.assertEquals("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", output.toString());
+        Assertions.assertEquals("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", output);
     }
 }
